a5beb4d9dd074188c04cf7b24742cafb479314e9
jiwoohong <uzihoon.dev@gmail.com>
2020-05-15 12:17:43 +0900

    change update body content way

--- libs/parsing-html.ts
+++ libs/parsing-html.ts
@@ -13,5 +13,5 @@ export function parsingHTML(
       <html lang=kor><head><meta charset="utf-8" /> <meta property="og:title" content="${title}" /> <meta property="og:description" content="${desc}" /> <meta name="description" content="${desc}" /> <meta property="og:type" content="website" /> <meta property="og:url" content="https://uzihoon.com/post/${id}" /> <meta property="og:site_name" content="Uzihoon" /> <meta name="twitter:description" content="${desc}" /> <meta name="twitter:title" content="${title}" />
     `
     )
-    .replace(/<body>/, `<body>${content}`);
+    .replace(/<\/body>/, `</body>${content}`);
 }
